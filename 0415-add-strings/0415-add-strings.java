8d19d1820c9d7b4c740f5b0333b248cbd4abd811
PERIYASAMY N <periyasamynatchimuthu@gmail.com>
2026-07-02 12:36:31 +0530

    Time: 19 ms (8.49%), Space: 45.8 MB (13.98%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,10 @@
+import java.math.*;
+class Solution {
+    public String addStrings(String num1, String num2) 
+    {
+        BigInteger a= new BigInteger(num1);
+        BigInteger b= new BigInteger(num2);
+    
+        return String.valueOf(a.add(b));
+    }
+}
